c33e6b8731c5bf4c6cf69f2698b70011d8ffe957
elyas <elyas.uii14@gmail.com>
2024-12-29 11:12:08 +0900

    Upload file for projek pertama PBO

---
+++ pbo_modul_satu.py
@@ -0,0 +1,12 @@
+# modul projek bersama PBO untuk latihan Mahasiswa
+# Mata Kulaih Pemrograman Berorientasi Objek (BPO)
+# Semua Mahasiswa diharuskan berkontribusi pada modul ini
+# Kontribusi berupa membuat class dan fungsi (lihat arahan lengkap di : 
+
+class test:
+    def __init__(self, m):
+        self.__m=m
+    
+    def mes(self):
+        return self.__m;
+
