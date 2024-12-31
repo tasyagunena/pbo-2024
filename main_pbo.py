0474ab4e25e70345932870b23f1b9ddb2fe299db
elyas <elyas.uii14@gmail.com>
2024-12-31 10:37:27 +0900

    Update keterangan in files tugas - p2

--- main_pbo.py
+++ main_pbo.py
@@ -3,5 +3,10 @@
 
 import modul_satu_pbo as msatu
 
+# ini contoh implementasi atau menguji fungsi dan class, versi saya
 p = msatu.test("Halo.. This is the MMMMM ");
 print ("\nTest Pesan : ", p.mes(), "\n")
+
+
+# silahkan uji fungsi dan class anda dibawah
+# pastikan class dan fungsi sudah anda buat di modul_satu_pbo.py
--- modul_satu_pbo.py
+++ modul_satu_pbo.py
@@ -1,7 +1,7 @@
 # modul projek bersama PBO untuk latihan Mahasiswa
 # Mata Kulaih Pemrograman Berorientasi Objek (BPO)
 # Semua Mahasiswa diharuskan berkontribusi pada modul ini
-# Kontribusi berupa membuat class dan fungsi (lihat arahan lengkap di : 
+# Kontribusi berupa membuat class dan fungsi (lihat arahan lengkap di : https://infoummu.github.io/pbo/
 
 # ini class test dari elyas: 
 class test:
@@ -11,3 +11,5 @@ class test:
     def mes(self):
         return self.__m;
 
+# silahkan lanjutkan dengan fungsi dan calss anda dibawah
+# pastikan untuk menguji class dan fungsi yang sudah di buat disini
