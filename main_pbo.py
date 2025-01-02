fc58b3254f503404e33d73acdaf93f8e38bbe8a6
firna23067 <firnasarifir@gmail.com>
2025-01-02 04:56:43 -0800

    main_pbo.py

--- main_pbo.py
+++ main_pbo.py
@@ -20,3 +20,23 @@ mahasiswa1.tambah_nilai("Fisika", 90)
 mahasiswa1.tambah_nilai("Algoritma", 78)
 
 mahasiswa1.tampilkan_info()
+
+#ini firna
+import modul_satu_pbo as firna67
+
+firna6= firna67.firna("firna","19","membaca","halteng","infomatika")
+firna6.proses()
+
+print("Ini Adalah Tambah")
+print(firna67.tambah(firna67.input3(),firna67.input3()))
+print("Ini Adalah Kali")
+print(firna67.kali(firna67.input3(),firna67.input3()))
+print("Ini Adalah Kurang")
+print(firna67.kurang(firna67.input3(),firna67.input3()))
+print("Ini Adalah Bagi")
+print(firna67.bagi(firna67.input3(),firna67.input3()))
+print("Ini Adalah Pangkat")
+print(firna67.pangkat(firna67.input3(),firna67.input3()))
+print("Ini Adalah Akar")
+print(firna67.akar(firna67.input3()))
+
