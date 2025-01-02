639884da6b090161659d512ed317e2da0cb829c0
fadel2791 <muhammadfadilnur64@gmail.com>
2025-01-02 16:09:59 +0700

    modul_satu_pbo.py

--- modul_satu_pbo.py
+++ modul_satu_pbo.py
@@ -44,3 +44,53 @@ class Mahasiswa:
         for item in self.nilai:
             print(f"  {item['mata_kuliah']}: {item['nilai']}")
         print(f"Rata-rata Nilai: {self.rata_rata_nilai():.2f}")
+
+
+#Ini Kontribusi Dari Muh. Fadel Nur
+class fadel:
+    def __init__ (self, nama, npm, kelas, alamat, asal):
+        self.n=nama
+        self.npm=npm
+        self.k=kelas
+        self.a=alamat
+        self.asal=asal
+
+    def output(self):
+        print(f"Nama Saya \t\t: {self.n} \nNpm Saya \t\t: {self.npm} \nKelas \t\t\t: {self.k} \nAlamat Saya Di \t\t: {self.a} \nAsal Saya Dari \t\t: {self.asal}")
+
+def fadel_0():
+    return fadel_1(int(input("Masukan Nilai Anda : ")))
+
+def fadel_1(nilai):
+    if nilai >= 80:
+        print("Nilai Yang Anda Peroleh Adalah : A")
+
+    elif nilai >=70:
+        print("Nilai Yang Anda Peroleh Adalah : B")
+
+    elif nilai >=60:
+        print("Nilai Yang Anda Peroleh Adalah : C")
+
+    elif nilai >=50:
+        print("Nilai Yang Anda Peroleh Adalah : D")
+
+    else:
+        print("Nilai Yang Anda Peroleh Adalah : E")
+
+    return fadel_3(str(input("Apakah Anda Ingin Mengulangi Program ( Y / T ) : ")))
+    
+def fadel_3(masukan):
+    if masukan == "Y":
+        return fadel_0()
+    
+    if masukan == "y":
+        return fadel_0()
+    
+    if masukan == "T":
+        print("Ini Adalah Akhir Program Terima Kasih Sudah Mencoba Program FADEL :) ")
+
+    if masukan == "t":
+        print("Ini Adalah Akhir Program Terima Kasih Sudah Mencoba Program FADEL :) ")
+
+    else:
+        print("Ini Adalah Akhir Program Terima Kasih Sudah Mencoba Program FADEL :) ")
