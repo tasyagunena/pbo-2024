65974f2b73b14877314c682ac66a4c5440aedaf2
FD-XI <elyas.uii14@gmail.com>
2025-01-02 16:41:51 +0900

    Ini Pull Request Test dari Ikhwan Elyas

--- modul_satu_pbo.py
+++ modul_satu_pbo.py
@@ -2,7 +2,7 @@
 # Mata Kulaih Pemrograman Berorientasi Objek (BPO)
 # Semua Mahasiswa diharuskan berkontribusi pada modul ini
 # Kontribusi berupa membuat class dan fungsi (lihat arahan lengkap di : https://infoummu.github.io/pbo/
-
+# .
 # ini class test dari elyas: 
 class test:
     def __init__(self, m):
