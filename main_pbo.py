7fb2ecedb96faa5b025a2bbc444dc06bf627488d
nurul23077 <fira14828@gmail.com>
2025-01-02 04:42:16 -0800

    main_pbo.py

--- main_pbo.py
+++ main_pbo.py
@@ -20,3 +20,23 @@ mahasiswa1.tambah_nilai("Fisika", 90)
 mahasiswa1.tambah_nilai("Algoritma", 78)
 
 mahasiswa1.tampilkan_info()
+
+#nurul
+import modul_satu_pbo as nurul0
+
+nurul1= nurul0.nurul("nurul","20","membaca","patani","infomatika")
+nurul1.proses()
+
+print("Ini Adalah Tambah")
+print(nurul0.tambah(nurul0.input3(),nurul0.input3()))
+print("Ini Adalah Kali")
+print(nurul0.kali(nurul0.input3(),nurul0.input3()))
+print("Ini Adalah Kurang")
+print(nurul0.kurang(nurul0.input3(),nurul0.input3()))
+print("Ini Adalah Bagi")
+print(nurul0.bagi(nurul0.input3(),nurul0.input3()))
+print("Ini Adalah Pangkat")
+print(nurul0.pangkat(nurul0.input3(),nurul0.input3()))
+print("Ini Adalah Akar")
+print(nurul0.akar(nurul0.input3()))
+
