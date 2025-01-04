85d76c546d0f4dd82929e0e19e81fd6b2cb2fe3d
Lastryyy <sulastriarahman3@gmail.com>
2025-01-04 10:08:11 +0900

    Update modul_satu_pbo.py

--- modul_satu_pbo.py
+++ modul_satu_pbo.py
@@ -341,3 +341,19 @@ def gita_bagi(a,b):
 # >>>>>>> main Sarif04
 # >>>>>>> main kardichen27
 # >>>>>>> main khumayra07
+
+#ini kontribusi dari sulastri
+# bunga.py
+
+class Bunga:
+    def __init__(self, nama, warna, jenis):
+        self.nama = nama
+        self.warna = warna
+        self.jenis = jenis
+
+    def deskripsi(self):
+        print(f"Bunga: {self.nama}, Warna: {self.warna}, Jenis: {self.jenis}")
+
+    def ubah_warna(self, warna_baru):
+        self.warna = warna_baru
+        print(f"Warna {self.nama} telah diubah menjadi: {self.warna}")
