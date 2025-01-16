a403d317b0cdee0c2e1ff12c4f924f6f4e003c2d
Dilasari77 <dilasarisyafrudin07@gmail.com>
2025-01-16 21:04:21 +0900

    Update uas_main_pbo.py

--- uas_main_pbo.py
+++ uas_main_pbo.py
@@ -14,6 +14,9 @@ if npm == "000":
 elif npm == "066":
     # silahkan isi 2 (dua) baris Maksimal untuk memanggil fungsinya Jelia
     fasasdfa() 
+elif npm == "062":
+    # silahkan isi 2 (dua) baris Maksimal untuk memanggil fungsinya Dila
+    apa() 
 else:
     # silahkan isi 2 (dua) baris Maksimal untuk memanggil fungsinya ???
     fasasdfa()
