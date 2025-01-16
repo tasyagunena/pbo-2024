de22f50896cfba55f78e665f61c203c5bc63fe2d
FD-XI <elyas.uii14@gmail.com>
2025-01-16 15:30:42 +0900

    Update uas_main_pbo.py v2

--- uas_main_pbo.py
+++ uas_main_pbo.py
@@ -1,14 +1,14 @@
 # code utama untuk memanggil fungsi dari UAS Mahasiswa
-# setiap Mahasiswa hanya boleh berkontribusi 2 baris untuk memanggil fungsinya 
+# setiap Mahasiswa hanya boleh berkontribusi 2 baris (maksimal) untuk memanggil fungsinya 
 import uas_pbo_npm as anty
 
 npm=input("Masukkan NPM (tiga digit) :")
-if npm == "072":
-  # silahkan isi 2 (dua) baris untuk memanggil fungsinya Anty 
+if npm == "000":
+  # silahkan isi 2 (dua) baris Maksimal untuk memanggil fungsinya ELyas 
   fall()
 elif npm == "066":
-  # silahkan isi 2 (dua) baris untuk memanggil fungsinya Jelia
-  fasasdfa()
+  # silahkan isi 2 (dua) baris Maksimal untuk memanggil fungsinya Jelia
+  fasasdfa() 
 else:
-    # silahkan isi 2 (dua) baris untuk memanggil fungsinya ???
+    # silahkan isi 2 (dua) baris Maksimal untuk memanggil fungsinya ???
   fasasdfa()
