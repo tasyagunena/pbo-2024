ce3d0ee96fda5fa6491ed9dac49c17bdf2901476
refalinadarwin <refadarwin@gmail.com>
2025-01-16 09:15:57 -0800

    Update uas_pbo_23071.py

--- 23071/uas_pbo_23071.py
+++ 23071/uas_pbo_23071.py
@@ -1,17 +1,23 @@
-# uas_main_pbo.py
+class Pantai:
+    def __init__(self, nama_pantai):
+        self.nama_pantai = nama_pantai
+    
+    def tampilkan(self):
+        print(f"Pantai {self.nama_pantai} sangat indah!")
 
-import sys
-sys.path.append("23071")  # Menambahkan folder yang berisi file uas_pbo_23072.py ke path
+# Daftar nama pantai di Sulamadaha
+pantai_sulamadaha = [
+    "Jikomalamo",
+    "Pantai Masirete",
+    "Talaganita",
+    "Hol"
+]
 
-# Mengimpor file uas_pbo_23072.py
-import uas_pbo_23071 as refalina  
+# Menampilkan semua pantai
+def pantai():
+    for nama_pantai in pantai_sulamadaha:
+        Pantai(nama_pantai).tampilkan()
 
-npm = input("Masukkan NPM (tiga digit) : ")
-
-if npm == "000":
-    print("Fungsi Elyas")
-elif npm == "071":
-    # Memanggil fungsi milik Refalina
-    refalina.tampilkan_pantai()
-else:
-    print("Fungsi belum terdaftar untuk NPM tersebut.")
+# Fungsi utama untuk menjalankan modul ini
+if __name__ == "__main__":
+    pantai()
