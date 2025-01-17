1d70e809024db237554f1f4576d8284463f62176
iqrarindani <iqrarindani25@gmail.com>
2025-01-17 19:19:54 +0900

    Update uas_pbo_23051.py

--- 23051/uas_pbo_23051.py
+++ 23051/uas_pbo_23051.py
@@ -1,1 +1,17 @@
-a
+class Elektronik:
+    def __init__(self, nama, merk, harga):
+        self.nama = nama
+        self.merk = merk
+        self.harga = harga
+
+    def deskripsi(self):
+        print(f"Elektronik: {self.nama}, Merk: {self.merk}, Harga: {self.harga}")
+    
+    def ubah_harga(self, harga_baru):
+        self.harga = harga_baru
+        print(f"harga{self. nama} telah diubah menjadi: {self. harga}")
+
+def elektronik():
+    elektronik1 = Elektronik("Oppo", "Samsung", "Iphone")
+    elektronik1.deskripsi()
+
