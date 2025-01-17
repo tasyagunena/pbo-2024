13d20863bd0af8e9b0a3efc4aedc13f4e247071a
sanana14 <evanugapi14@gmail.com>
2025-01-17 17:05:53 +0900

    Update uas_main_pbo.py

--- uas_main_pbo.py
+++ uas_main_pbo.py
@@ -32,6 +32,9 @@ elif npm == "093":
 elif npm == "071":
     # silahkan isi 2 (dua) baris Maksimal untuk memanggil fungsinya Refalina Darwin
      eflnz.pantai()
+elif npm == "060":
+    # silahkan isi 2 (dua) baris Maksimal untuk memanggil fungsinya Efan umagapi
+     buku.buku(),buku.buku2(),buku.buku3()
 else:
     # silahkan isi 2 (dua) baris Maksimal untuk memanggil fungsinya ???
     fasasdfa()
