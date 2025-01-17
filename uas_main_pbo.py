e9c26201b6abd5a225415fde9878648c51cb4ce7
mujakar <mujakasyfiudin@gmail.com>
2025-01-17 13:43:03 +0700

    Update uas_main_pbo.py

--- uas_main_pbo.py
+++ uas_main_pbo.py
@@ -32,6 +32,9 @@ elif npm == "093":
 elif npm == "071":
     # silahkan isi 2 (dua) baris Maksimal untuk memanggil fungsinya Refalina Darwin
      eflnz.pantai()
+ elif npm == "064":
+    # silahkan isi 2 (dua) baris Maksimal untuk memanggil fungsinya Refalina Darwin
+     batu.batu()
 else:
     # silahkan isi 2 (dua) baris Maksimal untuk memanggil fungsinya ???
     fasasdfa()
