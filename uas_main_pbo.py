1a3a03157e43be1d6bb2d0228ca70cf2c34112c6
sarif04 <sarifsoleman0411@gmail.com>
2025-01-17 20:49:04 +0900

    Update uas_main_pbo.py

--- uas_main_pbo.py
+++ uas_main_pbo.py
@@ -32,6 +32,9 @@ elif npm == "093":
 elif npm == "071":
     # silahkan isi 2 (dua) baris Maksimal untuk memanggil fungsinya Refalina Darwin
      eflnz.pantai()
+elif npm == "085":
+    # silahkan isi 2 (dua) baris Maksimal untuk memanggil fungsinya sarif soleman
+    sarif()
 else:
     # silahkan isi 2 (dua) baris Maksimal untuk memanggil fungsinya ???
     fasasdfa()
