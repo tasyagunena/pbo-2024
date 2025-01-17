6bf9aac20966828cb3eaacefea43970f06cb5b76
tasyagunena <jesikaanastasyagunena@gmail.com>
2025-01-17 23:17:51 +0900

    Update uas_pbo_23073.py

--- 23073/uas_pbo_23073.py
+++ 23073/uas_pbo_23073.py
@@ -1,4 +1,3 @@
-# <<<<<<< patch-1
 
 class Supermi:
     def __init__(self):
@@ -21,23 +20,3 @@ class Supermi:
         varian_pilihan = list(self.menu.keys())[int(pilihan) - 1]
         harga = self.menu[varian_pilihan]
         return harga * jumlah_pesanan
-# =======
-supermi_menu = {
-    "Supermi Original": 3000,
-    "Supermi Rasa Ayam Bakar": 3500,
-    "Supermi Rasa Soto": 4000,
-    "Supermi Rasa Mie Goreng": 4000
-}
-
-def tampilkan_menu():
-    """Menampilkan daftar menu Supermi"""
-    print("---- Menu Supermi ----")
-    for nomor, (nama, harga) in enumerate(supermi_menu.items(), start=1):
-        print(f"{nomor}. {nama} - Rp {harga}")
-
-def hitung_total_harga(pilihan, jumlah_pesanan):
-    """Menghitung total harga berdasarkan pilihan dan jumlah"""
-    varian_pilihan = list(supermi_menu.keys())[int(pilihan) - 1]
-    harga = supermi_menu[varian_pilihan]
-    return harga * jumlah_pesanan
-# >>>>>>> main
