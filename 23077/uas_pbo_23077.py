b3a323e8b9b831bc7e8c08de9f4707158e340558
nurul23077 <fira14828@gmail.com>
2025-01-17 00:11:48 -0800

    Update uas_pbo_23077.py

--- 23077/uas_pbo_23077.py
+++ 23077/uas_pbo_23077.py
@@ -1,10 +1,11 @@
-panjang = float(input("Masukkan panjang persegi panjang: "))
-lebar = float(input("Masukkan lebar persegi panjang: "))
-
-luas = panjang * lebar
-keliling = 2 * (panjang + lebar)
-
 def nurul():
+    panjang = float(input("Masukkan panjang persegi panjang: "))
+    lebar = float(input("Masukkan lebar persegi panjang: "))
+
+    luas = panjang * lebar
+    keliling = 2 * (panjang + lebar)
+
+
     print("\n=== Hasil Perhitungan ===")
     print(f"Luas persegi panjang: {luas}")
     print(f"Keliling persegi panjang: {keliling}")
